98008a01008390d66343de28713e6aba735eabda
jeiversosa01 <jeiversosa01@gmail.com>
2022-11-06 15:47:54 -0500

    Proyecto CRUD terminado sin manejo de errores

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -41,4 +41,12 @@ export class FormComponent implements OnInit {
     );
   }
 
+  update(): void {
+    this.clienteService.update(this.cliente)
+    .subscribe(cliente => {
+      this.router.navigate(['/clientes'])
+      swal.fire('Cliente Actualizado', `Cliente ${cliente.nombre} actualizado con exito!`, 'success')
+    })
+  }
+
 }
